949e94febdf63032f3b137a979228e3ef43f5330
Vitor Rezende <dev.rezendevitor@gmail.com>
2022-11-07 15:54:06 -0300

    Numbers and basic

---
+++ lib/basic_math.ex
@@ -0,0 +1,24 @@
+defmodule BasicMath do
+  @moduledoc """
+  Documentation for BasicMath
+  """
+  def add(addend_1, addend_2) do
+    addend_1 + addend_2
+  end
+
+  def subtract(minuend, subtrahend) do
+    minuend - subtrahend
+  end
+
+  def multiply(multiplier, multiplicand) do
+    multiplier * multiplicand
+  end
+
+  def divide(dividend, divisor) do
+    dividend / divisor
+  end
+
+  def round_up(value) do
+    Float.ceil(value, 2)
+  end
+end
